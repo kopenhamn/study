cd314d85c516cb91129a533e096986502b2eb9d9
Michael Feninets <carpinua@gmail.com>
2019-05-15 23:00:46 +0300

    JS drum kit some changes

--- JS/DrumKit/myTry/main.js
+++ JS/DrumKit/myTry/main.js
@@ -1,8 +1,18 @@
-var key;
-window.addEventListener('keydown', function(e) {
-    console.log(e.keyCode);
-    key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+function playSound(e) {
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    const sound = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
+    if(!sound) {return}
+    sound.currentTime = 0;
+    sound.play();
     key.classList.add('playing');
-    setTimeout(() => {key.classList.remove('playing')}, 300);
-    console.log(key);
-})
+}
+
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitioned', removeTransition));
+
+window.addEventListener('keydown', playSound)
