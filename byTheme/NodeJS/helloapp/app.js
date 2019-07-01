7a51ecc1d8131021028dda6187bd5145e126f850
Michael Feninets <mfeninets@gmail.com>
2019-07-01 23:04:48 +0300

    add http server

--- byTheme/NodeJS/helloapp/app.js
+++ byTheme/NodeJS/helloapp/app.js
@@ -1,16 +1,23 @@
-// const http = require('http');
-//
-// http.createServer(function(request,response) {
-//     response.end("Hello NodeJS");
-// }).listen(3000, "127.0.0.1", function() {
-//     console.log("server started listening on port 3000");
-// });
-const os = require("os");
-const greeting = require("./greeting");
+const http = require('http');
 
-// получим имя текущего пользователя
-let userName = os.userInfo().username;
+const users = {
+    admin: 'admin',
+    test: 'test'
+}
 
+http.createServer(function(request,response) {
 
-console.log(`Дата запроса: ${greeting.date}`);
-console.log(greeting.getMessage(userName));
+    // response.setHeader("Content-Type", 'Access-Control-Allow-Headers', "text/html; charset=utf-8;");
+    response.writeHead(200, {
+        'Access-Control-Allow-Origin' : '*',
+        'Access-Control-Allow-Headers': 'data'
+    });
+    if(request.url === "/users"){
+        response.write('HELLLLLLO');
+    } else {
+        response.write("<h5>USERS</h5>");
+    }
+    response.end();
+}).listen(4000, "127.0.0.1", function() {
+    console.log("server started listening on port 4000");
+});
