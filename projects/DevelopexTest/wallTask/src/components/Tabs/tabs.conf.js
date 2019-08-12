3531e26d4981c136fde1c67dd7adb6f644b768f5
Michael Feninets <mfeninets@gmail.com>
2019-08-12 23:44:23 +0300

    MB: Add Tabs component

---
+++ projects/DevelopexTest/wallTask/src/components/Tabs/tabs.conf.js
@@ -0,0 +1,6 @@
+export const panes = [
+    { id: 1, icon: 'fa fa-circle', name: 'Everything', active: 'active' },
+    { id: 2, icon: 'fa fa-comments', name: 'Commentary' },
+    { id: 3, icon: 'fa fa-chevron-circle-down', name: 'Events' },
+    { id: 4, icon: 'fa fa-exclamation-triangle', name: 'Alerts' }
+]
