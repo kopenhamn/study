b74428fef7860906bd103492f97fbfc2d66c7e47
Michael Feninets <mfeninets@gmail.com>
2019-08-13 12:00:36 +0300

    MD: Add comment form to MD, move fa styles

--- projects/DevelopexTest/wallTask/src/components/ManageBlock/ManageBlock.js
+++ projects/DevelopexTest/wallTask/src/components/ManageBlock/ManageBlock.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Tabs from 'components/Tabs';
+import CommentForm from 'components/CommentForm'
 import './styles.scss'
 
 export function ManageBlock() {
     return (
         <div className='manage-block'>
             <Tabs />
+            <CommentForm />
         </div>
     )
 }
