fd16c9f4f47e023153c7760c75b7c02ca114cb41
Michael Feninets <42581643+kopenhamn@users.noreply.github.com>
2019-08-13 09:13:17 +0300

    Dev (#15)
    * Add CRA structure (#14)
    
    * Change gitignore file
    
    * Restructure project
    
    * Add sass
    
    * Add sass
    
    * MB: Add Tab component
    
    * MB: Add Tabs component
    
    * MB: Add ManageBlock component with active tabs
    
    * MB: Add MB to App

---
+++ projects/DevelopexTest/wallTask/src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ManageBlock from 'components/ManageBlock';
+
+function App() {
+  return (
+    <div className="App">
+      <ManageBlock />
+    </div>
+  );
+}
+
+export default App;
---
+++ projects/DevelopexTest/wallTask/src/components/Tabs/Tabs.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { Tab } from './Tab/Tab';
+import { panes } from './tabs.conf';
+import './styles.scss'
+
+export class Tabs extends React.Component {
+    state = {
+        selectedTabId: 1
+    }
+
+    isActive = id => this.state.selectedTabId === id;
+
+    setActiveTab = selectedTabId => this.setState({ selectedTabId });
+
+    render() {
+        const tabs = panes.map(tab => <Tab 
+            key={tab.id} 
+            icon={tab.icon} 
+            name={tab.name} 
+            isActive={this.isActive(tab.id)}
+            onActiveTab={() => this.setActiveTab(tab.id)}
+        />);
+        console.log(this.state);
+
+        return (
+            <ul className="navigation">
+                { tabs }
+            </ul>
+        )
+    }
+}
