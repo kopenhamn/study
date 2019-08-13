033202107d538efa249d6fef6dadfb922dbd63dd
Michael Feninets <mfeninets@gmail.com>
2019-08-13 17:46:45 +0300

    MB: Add avatar to comment form

--- projects/DevelopexTest/wallTask/src/components/CommentForm/CommentForm.js
+++ projects/DevelopexTest/wallTask/src/components/CommentForm/CommentForm.js
@@ -1,36 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { PostButton, ShareButton, AttachButton } from './buttons';
 
-import './styles.scss'
+import defaultImage from './defaultImage.png';
+import './styles.scss';
 
 export class CommentForm extends React.Component {
+    static propTypes = {
+        userAvatar: PropTypes.string
+    };
+
+    static defaultProps = {
+        userAvatar: defaultImage
+    };
+
     state = {
         value: ''
     };
 
     cancelForm = () => {
-        this.setState({value: ''})
+        this.setState({ value: '' })
     };
 
     handleChange = event => {
-        this.setState({value: event.target.value});
+        this.setState({ value: event.target.value });
     };
 
     handleAttach = event => {
-        this.setState({value: ''})
+        this.setState({ value: '' })
         alert('Comment was attached: ' + this.state.value);
         event.preventDefault();
     };
 
     handleShare = event => {
-        this.setState({value: ''})
+        this.setState({ value: '' })
         alert('Comment was shared: ' + this.state.value);
         event.preventDefault();
     };
 
     handleSubmit = event => {
-        this.setState({value: ''})
+        this.setState({ value: '' })
         alert('Comment was sent: ' + this.state.value);
         event.preventDefault();
     };
@@ -38,9 +48,16 @@ export class CommentForm extends React.Component {
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
-                <label>
-                    <textarea className='comment_textarea' value={this.state.value} onChange={this.handleChange} />
-                </label>
+                <div className='textarea_container'>
+                    <img
+                        src={this.props.userAvatar}
+                        alt='user avatar'
+                        className='user-avatar'
+                    ></img>
+                    <label>
+                        <textarea className='comment_textarea' value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                </div>
                 <div className='comment_buttons'>
                     <PostButton post={this.handleSubmit} />
                     <ShareButton share={this.handleShare} />
