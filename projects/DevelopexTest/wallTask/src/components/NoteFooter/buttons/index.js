2f4bf64839579609143da100956ab3ee1ba270ac
Michael Feninets <mfeninets@gmail.com>
2019-08-14 20:11:07 +0300

    NB: Remove useless buttons folder from NoteFooter

--- projects/DevelopexTest/wallTask/src/components/NoteFooter/buttons/index.js
+++
@@ -1,5 +0,0 @@
-import { CommentButton } from './CommentButton';
-import { FollowButton } from './FollowButton';
-import { TrayButton } from './TrayButton';
-
-export { CommentButton, FollowButton, TrayButton };
