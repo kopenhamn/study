c807ec4fe61ec5c32a349da6626a1f1dccffed4c
Michael Feninets <mfeninets@gmail.com>
2019-08-14 19:57:54 +0300

    NB: Add CommentFooter and styles to comments

--- projects/DevelopexTest/wallTask/src/components/NoteContent/NoteContent.js
+++ projects/DevelopexTest/wallTask/src/components/NoteContent/NoteContent.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import Comment from 'components/Comment';
+import CommentFooter from 'components/CommentFooter';
+
 import events from 'mocks/events.json'
 
 import './styles.scss'
@@ -8,14 +10,21 @@ import './styles.scss'
 export function NoteContent() {
     return (
         <div className='note-content'>
-            {events.events.map(event => <div
+            {
+                events.events.map(event => <div
                     key={event.id}
                     className='note-container'
                 >
                     <i className="far fa-comment-alt"></i>
                     <div>
                         <Comment content={event} note={true} />
-                        {event.comments.map(comment => <Comment key={comment.id} content={comment} />)}
+                        <CommentFooter date={event.date} />
+                        {
+                            event.comments.map(comment => <div key={comment.id}>
+                                <Comment content={comment} />
+                                <CommentFooter date={comment.date} />
+                            </div>)
+                        }
                     </div>
                 </div>)
             }
