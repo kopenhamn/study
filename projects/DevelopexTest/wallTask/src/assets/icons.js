9c64a13d54319fbc49cbc302f2a213f8cb16ac7e
Michael Feninets <mfeninets@gmail.com>
2019-08-16 00:24:32 +0300

    NB: Add icon constants

---
+++ projects/DevelopexTest/wallTask/src/assets/icons.js
@@ -0,0 +1,4 @@
+export const notes = 'far fa-comment-alt';
+export const simples ='fas fa-play'
+export const alerts = 'fas fa-exclamation-triangle';
+export const anonnotations = 'fas fa-comment-alt';
